64c1ac248ab74ba0ccb462b8859202d64b903b2b
chirag punia <90308217+Chirag-Punia@users.noreply.github.com>
2024-12-21 15:05:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use("/api/stocks", stockRoutes);
 
 
 mongoose
-  .connect(process.env.MONGODB_)
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
